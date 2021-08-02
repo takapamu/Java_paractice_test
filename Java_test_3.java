7ea150cfee26bb20a9be7cd3113a4f08e1ec1cf7
takapamu <takapamukyary0129@gmail.com>
2021-08-02 22:36:09 +0900

    finish Java_test_3

---
+++ Java_test_3.java
@@ -0,0 +1,10 @@
+public class Java_test_3 {
+  public static void main(String[] args){
+    int x = 13;
+    int y = 17;
+
+    System.out.println("x = " + x);
+    System.out.println("y = " + y);
+    
+  }
+}
