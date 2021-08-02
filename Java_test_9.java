ff5d040af7f305d637a0adcc9d594249910b3f9a
takapamu <takapamukyary0129@gmail.com>
2021-08-02 23:05:41 +0900

    finish Java_test_9

---
+++ Java_test_9.java
@@ -0,0 +1,16 @@
+public class Java_test_9 {
+  public static void main(String[] args){
+    int x = 5;
+
+    x = x * 2;
+    System.out.println(x);
+
+    x = 5;
+    x = x * 3;
+    System.out.println(x);
+
+    x = 5;
+    x = x * 4;
+    System.out.println(x);
+  }
+}
