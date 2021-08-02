82de85578e17a664dc1feec80fc30c85d22e3c62
takapamu <takapamukyary0129@gmail.com>
2021-08-02 22:28:51 +0900

    finish Java_test_2

--- Java_test_1.java
+++ Java_test_1.java
@@ -1,4 +1,4 @@
-class Java_test1 {
+class Java_test_1 {
   public static void main(String[] args){
     System.out.println("Hello World");
   }
---
+++ Java_test_2.java
@@ -0,0 +1,6 @@
+public class Java_test_2 {
+  public static void main(String[] args){
+    int x = 11;
+    System.out.println(x);
+  }
+}
