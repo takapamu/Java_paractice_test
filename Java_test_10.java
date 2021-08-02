842a0e480a3be5d3189ee6b3ee71872b46c7c010
takapamu <takapamukyary0129@gmail.com>
2021-08-02 23:08:50 +0900

    finish Java_test_10

---
+++ Java_test_10.java
@@ -0,0 +1,15 @@
+public class Java_test_10 {
+  public static void main(String[] args){
+    int x = 5;
+    x = x;
+    System.out.println(x);
+
+    x = 5;
+    x = x * x;
+    System.out.println(x);
+
+    x = 5;
+    x = x * x * x;
+    System.out.println(x);
+  }
+}
