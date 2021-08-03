a22ba5b16263bf583372b59c74aa9e4e4695a534
takapamu <takapamukyary0129@gmail.com>
2021-08-03 22:09:09 +0900

    finish Java_test_12

---
+++ Java_test_12.java
@@ -0,0 +1,12 @@
+public class Java_test_12 {
+  public static void main(String[] args){
+
+    int x = 5;
+    x++;
+    System.out.println("インスメント演算子 = " + x);
+
+    x = 5;
+    x--;
+    System.out.println("デクリメント演算子 = " + x);
+  }
+}
