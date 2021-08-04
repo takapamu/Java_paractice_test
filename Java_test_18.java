f8d3198770dd6a12440bb8ad9951fe34662951fa
takapamu <takapamukyary0129@gmail.com>
2021-08-04 17:14:19 +0900

    finish Java_test_18

---
+++ Java_test_18.java
@@ -0,0 +1,13 @@
+import java.io.*;
+
+public class Java_test_18 {
+  public static void main(String[] args) throws IOException{
+
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+   int x = Integer.parseInt(br.readLine());  // 文字列から数値を得る
+
+    System.out.println(x);
+
+  }
+}
