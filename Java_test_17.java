2c9d7bc8c76241abb7b2787e9cb66945eb16843c
takapamu <takapamukyary0129@gmail.com>
2021-08-04 16:57:30 +0900

    finish Java_test_17

---
+++ Java_test_17.java
@@ -0,0 +1,12 @@
+import java.io.*;
+
+public class Java_test_17 {
+  public static void main(String[] args) throws IOException{
+
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    String s = br.readLine();
+
+    System.out.println(s);
+  }
+}
