3b343bd7998c90c5531bf189901a282163156e81
takapamu <takapamukyary0129@gmail.com>
2021-08-05 23:11:33 +0900

    finish Java_test_38

---
+++ Java_test_38.java
@@ -0,0 +1,15 @@
+public class Java_test_38 {
+  public static void main(String[] args){
+
+    int ans = 1;
+
+    int n = 7; //階乗を求める値
+
+    for(int i = 1; i <= n; ++i){
+      ans = ans * i;
+    }
+
+    System.out.println(ans);
+
+  }
+}
