30ff1373215c117f89b2e8831dbc75be7a3f0100
takapamu <takapamukyary0129@gmail.com>
2021-08-13 21:55:22 +0900

    first Java_test_62

---
+++ Java_test_62.java
@@ -0,0 +1,17 @@
+public class Java_test_6 {
+  public static void main(String[] args){
+
+    int kuku[][] = new int[9][];
+
+    //配列kukuに値を代入する
+
+    for(int i = 0; j < 9; i++)
+    {
+      for(int j = 0; j < 9; j++)
+        System.out.printf("%2d",kuku[i][j]);
+
+      System.out.println();
+    }
+
+  }
+}
