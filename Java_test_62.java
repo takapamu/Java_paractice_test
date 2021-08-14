db3ed1c3862241fffac5a78eca28b257272d74be
takapamu <takapamukyary0129@gmail.com>
2021-08-14 22:14:37 +0900

    finish Java_test_62

--- Java_test_62.java
+++ Java_test_62.java
@@ -1,14 +1,16 @@
-public class Java_test_6 {
-  public static void main(String[] args){
+public class Java_test_62 {
+  public static void main(String[] args) {
 
-    int kuku[][] = new int[9][];
+    int kuku[][] = new int[9][9];
 
-    //配列kukuに値を代入する
+    for (int i = 0; i < 9; i++) {
+      for (int j = 0; j < 9; j++)
+        kuku[i][j] = (i + 1) * (j + 1);
+    }
 
-    for(int i = 0; j < 9; i++)
-    {
-      for(int j = 0; j < 9; j++)
-        System.out.printf("%2d",kuku[i][j]);
+    for (int i = 0; i < 9; i++) {
+      for (int j = 0; j < 9; j++)
+        System.out.printf(kuku[i][j] + " ");
 
       System.out.println();
     }
