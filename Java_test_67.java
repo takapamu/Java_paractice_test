2b3be455421fcb0479f4bffd4bee0b1f40a871f1
takapamu <takapamukyary0129@gmail.com>
2021-08-16 22:30:24 +0900

    finish Java_test_67

---
+++ Java_test_67.java
@@ -0,0 +1,20 @@
+import java.io.*;
+
+public class Java_test_67 {
+  public static void main(String[] args) throws IOException{
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    int x = Integer.parseInt(br.readLine());
+    int y = Integer.parseInt(br.readLine());
+    int z = Integer.parseInt(br.readLine());
+
+    System.out.println(MaxValue(x,MaxValue(y,z)));
+  }
+
+  static int MaxValue(int n1,int n2){
+    if(n1 > n2)
+    return n1;
+    else
+    return n2;
+  }
+}
