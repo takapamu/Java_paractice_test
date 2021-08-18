561f4686b4b46a8a5ee9a7155d43956971019357
takapamu <takapamukyary0129@gmail.com>
2021-08-18 19:22:19 +0900

    finish Java_test_71

---
+++ Java_test_71.java
@@ -0,0 +1,18 @@
+public class Java_test_71 {
+  public static void main(String[] args) {
+
+    for(int n = 10000; n < 10100; n++)
+    if(IsPrimeNumber(n))
+    System.out.print( n + " ");
+  }
+
+  static boolean IsPrimeNumber(int num){
+    if( num <= 3)
+      return true;
+    for( int i = 2; i <= (num / 2); i++)
+    if((num % i) == 0)
+      return false;
+
+    return true;
+  }
+}
